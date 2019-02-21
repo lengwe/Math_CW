29f453b9c2aaeefaffeda91badd3705853f6fa5b
lengwe <44134544+lengwe@users.noreply.github.com>
2019-02-21 10:49:04 +0000

    Update RK2_script.m

--- RL_Circuit/RK2_script.m
+++ RL_Circuit/RK2_script.m
@@ -7,7 +7,9 @@ L = 0.0015
 h = 0.005
 c = 0.00015
 V = 3.5
-%Vin = @(t) V*exp(-t^2/c);        %function of t
-Vin = 3.5
 
-[t,vout] = heun(Vin, tf, ti, ii, R, L,h);
+Vin =  @(t) V*exp(-t^2/c);
+%Vin = 3.5
+func = @(t,i) 1/L*(V*exp(-t^2/c)-R*i);               %Li'(t)+Ri(t)=Vin(t)
+
+[t,vout] = heun(func, Vin, tf, ti, ii, R, L,h);          
